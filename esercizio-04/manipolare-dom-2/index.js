f24589d38fe681eee94b67eeaea2d91666ef9d4b
Astrid <recoltyy@gmail.com>
2024-05-20 16:31:43 +0200

    added changes

--- esercizio-04/manipolare-dom-2/index.js
+++ esercizio-04/manipolare-dom-2/index.js
@@ -1,6 +1,3 @@
-//Recuperare il valore di ciascun campo di input  
-//e creare un oggetto person contenente le proprietà: firstName, lastName e age.
-
 const firstName = document.getElementById("firstName").value;
 const lastName = document.getElementById("lastName").value;
 const age = document.getElementById("age").value;
@@ -9,14 +6,5 @@ const person = { firstName, lastName, age };
 
 console.log(person);
 
-
-//Infine recuperare l'elemento form
-
-const formId = document.querySelector("form");
-formId.setAttribute("id", "my-form");
-
-const form = document.getElementById("my-form");
-
-//aggiungere l'attributo data-person contenente l'oggetto person in formato json
-
+const form = document.querySelector("form");
 form.setAttribute("data-person", JSON.stringify(person));
